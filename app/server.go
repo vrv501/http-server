23c261686e8d7c100cdc228f12b6facab23c5798
vrv501 <33621393+vrv501@users.noreply.github.com>
2024-07-31 05:23:23 +0000

    respond with 200

--- app/server.go
+++ app/server.go
@@ -18,8 +18,15 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
