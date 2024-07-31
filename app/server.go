3796356ad8e9267a3c6a044fbdb6aa6d7c0d8f1d
vrv501 <33621393+vrv501@users.noreply.github.com>
2024-07-31 09:46:44 +0000

    add support for post

--- app/server.go
+++ app/server.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -16,7 +17,6 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
 	flag.StringVar(&filesDir, "directory", "", "directory name to search for files")
 	flag.Parse()
 
@@ -52,8 +52,11 @@ func handleConnection(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	writer := bufio.NewWriter(conn)
 	var (
-		line []byte
-		err  error
+		line   []byte
+		err    error
+		method string
+		path   string
+		resp   string
 	)
 
 	/*
@@ -65,89 +68,42 @@ func handleConnection(conn net.Conn) {
 	line, _, err = reader.ReadLine() //reads until \r\n or \n is encountered
 	if err != nil {
 		fmt.Println(err)
+		resp = createErrResponse(err)
+		sendResponse(writer, resp)
 		return
 	}
 
-	var (
-		resp string
-		path string
-	)
-	req := strings.Split(string(line), " ") // metadata is seperated by white-space
-	if len(req) > 1 {
-		path = req[1]
-	}
-
-	if path == "/" {
-		resp = createHTTPResponse(200, map[string]string{}, "")
-	} else if subPath, hasPrefix := strings.CutPrefix(path, "/echo/"); hasPrefix { // /echo/{str} should return str as response
-		resp = createHTTPResponse(200, map[string]string{
-			"Content-Type":   "text/plain",
-			"Content-Length": fmt.Sprintf("%d", len(subPath))},
-			subPath)
-	} else if strings.HasSuffix(path, "/user-agent") { // /user-agent should return header-value of User-Agent as response
-		var (
-			userAgent   string
-			prefixFound bool
-			lineStr     string
-		)
-
-		// each header is of format: key: value\r\n
-		for {
-			line, _, err = reader.ReadLine()
-			if err != nil {
-				fmt.Println(err)
-				break
-			}
-
-			lineStr = string(line)
-			if lineStr == "" { // when we have read all-lines, ReadLine returns empty-line
-				break
-			}
-			userAgent, prefixFound = strings.CutPrefix(lineStr, "User-Agent: ")
-			if prefixFound {
-				break
-			}
-		}
-
-		if err != nil {
-			errStr := err.Error()
-			resp = createHTTPResponse(500, map[string]string{
-				"Content-Type":   "text/plain",
-				"Content-Length": fmt.Sprintf("%d", len(errStr))},
-				errStr)
-		} else {
-			if userAgent == "" {
-				resp = createHTTPResponse(404, map[string]string{}, "")
-			} else {
-				resp = createHTTPResponse(200, map[string]string{
-					"Content-Type":   "text/plain",
-					"Content-Length": fmt.Sprintf("%d", len(userAgent))},
-					userAgent)
-			}
-		}
-	} else if fileName, hasPrefix := strings.CutPrefix(path, "/files/"); hasPrefix { // read file and send its content as response
-		file, err := os.ReadFile(filesDir + fileName)
-		if err != nil {
-			if errors.Is(err, os.ErrNotExist) {
-				resp = createHTTPResponse(404, map[string]string{}, "")
-			} else {
-				errStr := err.Error()
-				resp = createHTTPResponse(500, map[string]string{
-					"Content-Type":   "text/plain",
-					"Content-Length": fmt.Sprintf("%d", len(errStr))},
-					errStr)
-			}
-		} else {
-			resp = createHTTPResponse(200, map[string]string{
-				"Content-Type":   "application/octet-stream",
-				"Content-Length": fmt.Sprintf("%d", len(file))},
-				string(file))
-		}
+	metadata := strings.Split(string(line), " ") // metadata is seperated by white-space
+	if len(metadata) > 1 {                       // metadata should atleast have 2 strings
+		path = metadata[1]
+		method = metadata[0]
 	} else {
-		resp = createHTTPResponse(404, map[string]string{}, "")
+		errMsg := "invalid metadata"
+		resp = createHTTPResponse(400, map[string]string{
+			"Content-Type":   "text/plain",
+			"Content-Length": fmt.Sprintf("%d", len(errMsg))},
+			errMsg)
+		sendResponse(writer, resp)
+		return
 	}
 
-	_, err = writer.WriteString(resp)
+	if method == "GET" {
+		resp = handleGETMethod(path, reader)
+	} else if method == "POST" {
+		resp = handlePOSTMethod(path, reader)
+	} else {
+		errMsg := fmt.Sprintf("invalid method: %s", method)
+		resp = createHTTPResponse(400, map[string]string{
+			"Content-Type":   "text/plain",
+			"Content-Length": fmt.Sprintf("%d", len(errMsg))},
+			errMsg)
+	}
+
+	sendResponse(writer, resp)
+}
+
+func sendResponse(writer *bufio.Writer, resp string) {
+	_, err := writer.WriteString(resp)
 	if err != nil {
 		fmt.Println("Error writing stringResp:", err.Error())
 		return
@@ -158,6 +114,15 @@ func handleConnection(conn net.Conn) {
 		fmt.Println("Error writing to connection:", err.Error())
 		return
 	}
+}
+
+func createErrResponse(err error) string {
+
+	errMsg := err.Error()
+	return createHTTPResponse(500, map[string]string{
+		"Content-Type":   "text/plain",
+		"Content-Length": fmt.Sprintf("%d", len(errMsg))},
+		errMsg)
 }
 
 func createHTTPResponse(statusCode int, headers map[string]string, requestBody string) string {
@@ -174,6 +139,10 @@ func createHTTPResponse(statusCode int, headers map[string]string, requestBody s
 	switch statusCode {
 	case 200:
 		statusCodeStr = "OK"
+	case 201:
+		statusCodeStr = "Created"
+	case 400:
+		statusCodeStr = "Bad Request"
 	case 404:
 		statusCodeStr = "Not Found"
 	case 500:
@@ -181,4 +150,139 @@ func createHTTPResponse(statusCode int, headers map[string]string, requestBody s
 	}
 
 	return fmt.Sprintf("HTTP/1.1 %d %s\r\n%s\r\n%s", statusCode, statusCodeStr, headerStr.String(), requestBody)
+}
+
+func handlePOSTMethod(path string, reader *bufio.Reader) string {
+
+	fileName, hasPrefix := strings.CutPrefix(path, "/files/")
+	if !hasPrefix {
+		errStr := "invalidPath: " + path
+		return createHTTPResponse(400, map[string]string{
+			"Content-Type":   "text/plain",
+			"Content-Length": fmt.Sprintf("%d", len(errStr))},
+			errStr)
+	}
+
+	headers, err := readReqHeaders(reader)
+	if err != nil {
+		fmt.Println(err)
+		return createErrResponse(err)
+	}
+
+	contentLengthStr, ok := headers["Content-Length"]
+	if !ok {
+		errStr := "content-length not found"
+		return createHTTPResponse(400, map[string]string{
+			"Content-Type":   "text/plain",
+			"Content-Length": fmt.Sprintf("%d", len(errStr))},
+			errStr)
+	}
+
+	contentLength, err := strconv.ParseUint(contentLengthStr, 10, 64)
+	if err != nil {
+		errStr := err.Error()
+		return createHTTPResponse(400, map[string]string{
+			"Content-Type":   "text/plain",
+			"Content-Length": fmt.Sprintf("%d", len(errStr))},
+			errStr)
+	}
+
+	inpSlice := make([]byte, contentLength)
+	_, err = reader.Read(inpSlice)
+	if err != nil {
+		fmt.Println(err)
+		return createErrResponse(err)
+	}
+
+	// create file
+	f, err := os.Create(filesDir + fileName)
+	if err != nil {
+		fmt.Println(err)
+		return createErrResponse(err)
+	}
+	defer f.Close()
+
+	_, err = f.Write(inpSlice)
+	if err != nil {
+		fmt.Println(err)
+		return createErrResponse(err)
+	}
+
+	return createHTTPResponse(201, map[string]string{}, "")
+}
+
+func handleGETMethod(path string, reader *bufio.Reader) string {
+
+	if path == "/" {
+		return createHTTPResponse(200, map[string]string{}, "")
+	} else if subPath, hasPrefix := strings.CutPrefix(path, "/echo/"); hasPrefix { // /echo/{str} should return str as response
+		return createHTTPResponse(200, map[string]string{
+			"Content-Type":   "text/plain",
+			"Content-Length": fmt.Sprintf("%d", len(subPath))},
+			subPath)
+	} else if strings.HasSuffix(path, "/user-agent") { // /user-agent should return header-value of User-Agent as response
+
+		headers, err := readReqHeaders(reader)
+		if err != nil {
+			fmt.Println(err)
+			return createErrResponse(err)
+		}
+
+		userAgent, ok := headers["User-Agent"]
+		if !ok {
+			return createHTTPResponse(404, map[string]string{}, "")
+		}
+
+		return createHTTPResponse(200, map[string]string{
+			"Content-Type":   "text/plain",
+			"Content-Length": fmt.Sprintf("%d", len(userAgent))},
+			userAgent)
+	} else if fileName, hasPrefix := strings.CutPrefix(path, "/files/"); hasPrefix { // read file and send its content as response
+		file, err := os.ReadFile(filesDir + fileName)
+		if err != nil {
+			if errors.Is(err, os.ErrNotExist) {
+				return createHTTPResponse(404, map[string]string{}, "")
+			}
+
+			fmt.Println(err)
+			return err.Error()
+		}
+
+		return createHTTPResponse(200, map[string]string{
+			"Content-Type":   "application/octet-stream",
+			"Content-Length": fmt.Sprintf("%d", len(file))},
+			string(file))
+	}
+
+	return createHTTPResponse(404, map[string]string{}, "")
+}
+
+func readReqHeaders(reader *bufio.Reader) (map[string]string, error) {
+
+	var (
+		lineStr string
+		headers = map[string]string{}
+	)
+
+	// each header is of format: key: value\r\n
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			return nil, err
+		}
+
+		lineStr = string(line)
+		if lineStr == "" { // when we have read all-lines, ReadLine returns empty-line
+			break
+		}
+
+		keyValue := strings.Split(lineStr, ": ")
+		if len(keyValue) != 2 {
+			return nil, errors.New("invalid header found")
+		}
+
+		headers[strings.TrimSpace(keyValue[0])] = strings.TrimSpace(keyValue[1])
+	}
+
+	return headers, nil
 }
